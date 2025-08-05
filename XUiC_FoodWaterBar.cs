bd6e7fe2bb3bbb18548555bdb5bd8b705a800c1f
synrbb <192501796+synrbb@users.noreply.github.com>
2025-08-05 23:51:07 +0900

    Use statcurrentWMaxFormatterAOfB

--- XUiC_FoodWaterBar.cs
+++ XUiC_FoodWaterBar.cs
@@ -4,10 +4,6 @@ namespace LWFBars
 {
     public class XUiC_FoodWaterBar : XUiC_HUDStatBar
     {
-        private readonly CachedStringFormatter<int, int> FoodFormatter = new((a, b) => $"{a}/{b}");
-
-        private readonly CachedStringFormatter<int, int> WaterFormatter = new((a, b) => $"{a}/{b}");
-
         public override bool GetBindingValue(ref string value, string bindingName)
         {
             if (bindingName == "statcurrentwithmax")
@@ -18,14 +14,14 @@ namespace LWFBars
                         if (LocalPlayer != null)
                         {
                             Stat stat = LocalPlayer.Stats.Food;
-                            value = FoodFormatter.Format((int)Math.Round(stat.Value), (int)Math.Round(stat.Max));
+                            value = statcurrentWMaxFormatterAOfB.Format((int)Math.Round(stat.Value), (int)Math.Round(stat.Max));
                         }
                         return true;
                     case HUDStatTypes.Water:
                         if (LocalPlayer != null)
                         {
                             Stat stat = LocalPlayer.Stats.Water;
-                            value = WaterFormatter.Format((int)Math.Round(stat.Value), (int)Math.Round(stat.Max));
+                            value = statcurrentWMaxFormatterAOfB.Format((int)Math.Round(stat.Value), (int)Math.Round(stat.Max));
                         }
                         return true;
                 }
