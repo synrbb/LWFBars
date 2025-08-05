daa9205f0be062b58953673b0a46e0ea19ae6614
synrbb <192501796+synrbb@users.noreply.github.com>
2025-08-06 00:29:03 +0900

    Patch XUiC_HUDStatBar.HasChanged for food and water bars

--- ModInit.cs
+++ ModInit.cs
@@ -1,9 +1,13 @@
+using HarmonyLib;
+
 namespace LWFBars
 {
     public class ModInit : IModApi
     {
         public void InitMod(Mod _modInstance)
         {
+            var harmony = new Harmony("io.github.synrbb.LWFBars");
+            harmony.PatchAll();
         }
     }
 }
---
+++ Patch_XUiC_HUDStatBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace LWFBars
+{
+    [HarmonyPatch(typeof(XUiC_HUDStatBar))]
+    class Patch_XUiC_HUDStatBar
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(XUiC_HUDStatBar.HasChanged))]
+        static IEnumerable<CodeInstruction> HasChanged(IEnumerable<CodeInstruction> instructions)
+        {
+            var oldGetter = typeof(Stat).GetProperty(nameof(Stat.ValuePercentUI)).GetGetMethod();
+            var newGetter = typeof(Stat).GetProperty(nameof(Stat.Value)).GetGetMethod();
+            var statFields = new FieldInfo[]
+            {
+                typeof(EntityStats).GetField(nameof(EntityStats.Water)),
+                typeof(EntityStats).GetField(nameof(EntityStats.Food))
+            };
+
+            var codeMatcher = new CodeMatcher(instructions);
+
+            foreach (var statField in statFields)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    codeMatcher.MatchStartForward(
+                            new CodeMatch(OpCodes.Ldfld, statField),
+                            new CodeMatch(OpCodes.Callvirt, oldGetter)
+                        )
+                        .ThrowIfInvalid($"failed to match {i} for {statField}")
+                        .InstructionAt(1).operand = newGetter;
+                }
+            }
+
+            return codeMatcher.Instructions();
+        }
+    }
+}
